73ed9aee2748a2865481ca81b1a27ef9dd11daaf
Ravi Bhadeshiya <rnbhadeshiya@gmail.com>
2018-02-21 18:37:32 -0500

    qual1a complete and code clean up!

--- README.md
+++ README.md
@@ -19,11 +19,11 @@ $ catkin_make
 ```
 
 
-TO run qulifier1 type:
+To run qulifier1 type:
 ```
-$ rosrun osrf_gear gear.py -f `catkin_find --share osrf_gear`/config/qual1a.yaml ~/catkin_ws/src/project_ariac/config/team_conf.yaml 
+$ rosrun osrf_gear gear.py -f `catkin_find --share osrf_gear`/config/qual1a.yaml ~/catkin_ws/src/project_ariac/config/qual1a_config.yaml 
 ```
 and in new terminal type:
 ```
-$ rosrun project_ariac project_ariac_node
+$ roslaunch project_ariac project_ariac_qual_1a.launch
 ```
--- src/UR10_Control.cpp
+++ src/UR10_Control.cpp
@@ -4,54 +4,62 @@
  * @version    0.1
  * @brief      Class for controlling ur10 arm
  *
- * @copyright  MIT License (c) 2017 Ravi Bhadeshiya
+ * @copyright  BSD 3-Clause License (c) 2018 Ravi Bhadeshiya
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+ * Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its contributors may
+be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include "project_ariac/UR10_Control.hpp"
 
 UR10_Control::UR10_Control() : ur10_("manipulator") {
   // ur10_.setPlanningTime(5);
-  ur10_.setPlanningTime(20);
+  ur10_.setPlanningTime(2.5);
   ur10_.setNumPlanningAttempts(20);
   ur10_.setPlannerId("RRTConnectkConfigDefault");
 
   ros::Duration(1.0).sleep();
+
   this->goToStart();
 
   auto transform = this->getTransfrom("/world", "/ee_link");
 
-  home.orientation.x = transform.getRotation().x();
-  home.orientation.y = transform.getRotation().y();
-  home.orientation.z = transform.getRotation().z();
-  home.orientation.w = transform.getRotation().w();
-  target = home;
+  home_.orientation.x = transform.getRotation().x();
+  home_.orientation.y = transform.getRotation().y();
+  home_.orientation.z = transform.getRotation().z();
+  home_.orientation.w = transform.getRotation().w();
+  target_ = home_;
 
-  gripper_ = nh.serviceClient<osrf_gear::VacuumGripperControl>(
+  gripper_ = nh_.serviceClient<osrf_gear::VacuumGripperControl>(
       "/ariac/gripper/control");
 
   transform = this->getTransfrom("/world", "/agv1_load_point_frame");
 
-  agv.position.x = transform.getOrigin().x();
-  agv.position.y = transform.getOrigin().y();
-  agv.position.z = transform.getOrigin().z() + 2 * z_offSet_pickUp;
-  agv.orientation = home.orientation;
+  agv_.position.x = transform.getOrigin().x();
+  agv_.position.y = transform.getOrigin().y();
+  agv_.position.z = transform.getOrigin().z() + 2 * z_offSet_pickUp_;
+  agv_.orientation = home_.orientation;
 }
 
 UR10_Control::~UR10_Control() {}
@@ -71,16 +79,16 @@ tf::StampedTransform UR10_Control::getTransfrom(const std::string& src,
   return transform;
 }
 
-void UR10_Control::set_target(const geometry_msgs::Pose& target_) {
-  target.position = target_.position;
-  target.position.z += z_offSet_pickUp;
-  ur10_.setPoseTarget(target);
+void UR10_Control::setTarget(const geometry_msgs::Pose& target) {
+  target_.position = target.position;
+  target_.position.z += z_offSet_pickUp_;
+  ur10_.setPoseTarget(target_);
 }
 
 bool UR10_Control::plan() {
   // bool success = ur10_.plan(planner);
   ROS_INFO("Planning start!");
-  bool success = (ur10_.plan(planner) ==
+  bool success = (ur10_.plan(planner_) ==
                   moveit::planning_interface::MoveItErrorCode::SUCCESS);
   if (success)
     ROS_INFO("Planned success.");
@@ -90,7 +98,7 @@ bool UR10_Control::plan() {
 }
 
 void UR10_Control::move() {
-  ros::AsyncSpinner spinner(1);
+  ros::AsyncSpinner spinner(4);
   spinner.start();
   if (this->plan()) {
     ur10_.move();
@@ -107,7 +115,7 @@ void UR10_Control::goToStart() {
   ros::Duration(1.0).sleep();
 }
 void UR10_Control::place() {
-  ROS_INFO_STREAM(agv.position);
+  // ROS_INFO_STREAM(agv.position);
 
   std::vector<std::vector<double>> poses = {
       {1.76, 0.38, -1.38, 2.76, 3.27, -1.51, 0.00}};  //,
@@ -121,7 +129,7 @@ void UR10_Control::place() {
     ros::Duration(1.0).sleep();
   }
 
-  this->set_target(agv);
+  this->setTarget(agv_);
   this->move();
 
   this->gripperAction(gripper::OPEN);
@@ -133,12 +141,8 @@ void UR10_Control::place() {
     this->move();
     ros::Duration(1.0).sleep();
   }
-
-
-  //   ur10_.setJointValueTarget({0, 1.56, -1.38, 1.5, 3.27, -1.51, 0.00});
-  //   this->move();
-  //   ros::Duration(1).sleep();
 }
+
 void UR10_Control::gripperAction(const bool action) {
   osrf_gear::VacuumGripperControl srv;
 
@@ -154,22 +158,21 @@ void UR10_Control::gripperAction(const bool action) {
     ROS_ERROR("Gripper Action Failed!");
 }
 
-
 void UR10_Control::pickAndPlace(const geometry_msgs::Pose& target_) {
-       // ur10.goToStart();
-    ROS_INFO("Setting Target..");
-    this->set_target(target_);
-    ROS_INFO("Moving");
-    this->move();
-    ROS_INFO("Picking Up");
+  // ur10.goToStart();
+  ROS_INFO("Setting Target..");
+  this->setTarget(target_);
+  ROS_INFO("Moving");
+  this->move();
+  ROS_INFO("Picking Up");
 
-    this->gripperAction(gripper::CLOSE);
-    ROS_INFO("Placing");
+  this->gripperAction(gripper::CLOSE);
+  ROS_INFO("Placing");
 
-    this->goToStart();
+  this->goToStart();
 
-    this->place();
-    // ur10.gripperAction(gripper::OPEN);
-    ROS_INFO("Home");
-    this->goToStart();
+  this->place();
+
+  ROS_INFO("Home");
+  this->goToStart();
 }
