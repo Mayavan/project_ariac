f39ba5044e1c8f32e2345a7fd265c58c36018a68
harish <harish1696@gmail.com>
2018-04-20 17:00:05 -0400

    [Not verified] accounting for the orientation of the part ehile placing on the tray.

--- src/UR10_Control.cpp
+++ src/UR10_Control.cpp
@@ -228,7 +228,19 @@ bool UR10_Control::place(geometry_msgs::Pose target, int agv) {
   target.position.z += 2 * z_offSet_;
   // TODO(harish) final orientance incorrect (Just yaw fix)
   // orientation convert to rpy and assign yaw and convert back to euler
-  target.orientation = home_.orientation;
+  tf::Quaternion q_h(home_.orientation.x, home_.orientation.y, home_.orientation.z, home_.orientation.w);
+  tf::Matrix3x3 m_h(q_h);
+  double r_h, r_t, p_h, p_t, y_h, y_t;
+  m_h.getRPY(r_h, p_h, y_h);
+  tf::Quaternion q_t(target.orientation.x, target.orientation.y, target.orientation.z, target.orientation.w);
+  tf::Matrix3x3 m_t(q_t);
+  m_t.getRPY(r_t, p_t, y_t);
+  tf::Quaternion final;
+  final = tf::createQuaternionFromRPY(r_h, p_h, y_t);
+  target.orientation.x = final.getX();
+  target.orientation.y = final.getY();
+  target.orientation.z = final.getZ();
+  target.orientation.w = final.getW();
   waypoints.push_back(target);
 
   // it will stop the motion,
